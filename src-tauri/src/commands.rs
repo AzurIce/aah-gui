4171ec2d2b49a12dbdb5d106d2ff3c8809d00258
tjw <114938661+tjw2003@users.noreply.github.com>
2024-06-29 17:09:42 +0800

    测试

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,4 +1,4 @@
-use std::io::Cursor;
+use std::{io::Cursor, path::Path};
 
 use aah_core::AAH;
 use image::ImageFormat;
@@ -8,7 +8,7 @@ use crate::state::core_instance;
 #[tauri::command]
 pub fn connect(serial: String) -> Result<(), String> {
     let mut core = core_instance().lock().unwrap();
-    let connected_aah = AAH::connect(serial,"./resource").map_err(|err| format!("{}", err))?;
+    let connected_aah = AAH::connect(serial, "./resources/").map_err(|err| format!("{}", err))?;
     *core = Some(connected_aah);
     Ok(())
 }
@@ -29,4 +29,36 @@ pub fn get_screen() -> Result<Vec<u8>, String> {
         .map_err(|e| format!("编码图像失败: {:?}", e))?;
 
     Ok(buf)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn foo() {
+        let serial = String::from("127.0.0.1:16384");
+
+        // 调用 connect 函数进行连接
+        let result = connect(serial.clone());
+
+        // 验证连接操作的结果
+        match result {
+            Ok(()) => {
+                println!("Device connected successfully!");
+                // 这里可以添加更多的断言或验证连接后的操作
+            }
+            Err(err) => {
+                eprintln!("Failed to connect to device: {}", err);
+                // 在连接失败的情况下，可以记录错误信息或者处理其他逻辑
+                panic!("Test failed: Connection error");
+            }
+        }
+
+        // 调用 get_screen() 函数获取屏幕数据
+        let result1 = get_screen();
+
+        // 这是个sb操作，保持注释就好
+        // println!("{:?}", result1);
+    }
 }
