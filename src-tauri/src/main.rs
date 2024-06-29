665f6d8fab865add5e8ea51c2ff4e425e3f048d8
Azur冰弦 <973562770@qq.com>
2024-06-29 11:08:39 +0800

    feat: basic get_image() tauri command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,72 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use image::{ImageBuffer, ImageFormat};
+use std::io::{Cursor, Write};
+use num_complex::Complex; 
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+#[tauri::command]
+fn get_image() -> Result<Vec<u8>, String> {
+    let imgx = 800;
+    let imgy = 800;
+
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = ImageBuffer::new(imgx, imgy);
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+
+    // A redundant loop to demonstrate reading image data
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
+
+            let c = num_complex::Complex::new(-0.4, 0.6);
+            let mut z = num_complex::Complex::new(cx, cy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let image::Rgb(data) = *pixel;
+            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+        }
+    }
+
+    // 创建一个内存缓冲区，用于写入图像数据
+    let mut buf = Vec::new();
+
+    // 使用 Cursor 包装 buf，使其实现了 Seek trait
+    let mut cursor = Cursor::new(&mut buf);
+
+    // 将图像数据以 JPEG 格式写入到 buf 中
+    imgbuf.write_to(&mut cursor, ImageFormat::Jpeg).map_err(|e| format!("编码图像失败: {:?}", e))?;
+
+    // 将包含图像数据的 Vec<u8> 返回给前端
+    Ok(buf)
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,get_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
